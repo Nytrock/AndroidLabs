2bc72dec826a718a3d6d551ed89827a71735becd
Nytrock <savvazhirnov@gmail.com>
2025-10-08 18:10:26 +0300

    Redo lab 4 and start lab 5

---
+++ app/lab4/src/main/java/com/example/lab4/DataActivity.java
@@ -0,0 +1,58 @@
+package com.example.lab4;
+
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DataActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data);
+
+        Intent intent = getIntent();
+        TextView buffer;
+
+        buffer = findViewById(R.id.textResult);
+        buffer.setText(intent.getStringExtra("text"));
+
+        buffer = findViewById(R.id.numberResult);
+        int number = intent.getIntExtra("number", 0);
+        buffer.setText(String.valueOf(number));
+
+        buffer = findViewById(R.id.dataResult);
+
+        if (intent.getBooleanExtra("showDate", false)){
+            buffer.setVisibility(VISIBLE);
+        } else {
+            buffer.setVisibility(INVISIBLE);
+        }
+
+        Date currentTime = Calendar.getInstance().getTime();
+        String dateText = getString(R.string.date_result, currentTime.toString());
+        buffer.setText(dateText);
+    }
+
+    public void returnData(View view) {
+        EditText textInput = findViewById(R.id.messageEdit);
+        String message = textInput.getText().toString();
+
+        if (message.isEmpty())
+            return;
+
+        Intent result = new Intent();
+        result.putExtra("message", message);
+        setResult(RESULT_OK, result);
+        finish();
+    }
+}
--- app/lab5/src/main/java/com/example/lab5/MainActivity.java
+++ app/lab5/src/main/java/com/example/lab5/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.lab5;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private String socialStatus = "Студент";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextBirthday = findViewById(R.id.editTextBirthday);
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("Name", editTextName.getText().toString());
+        intent.putExtra("Birthday", editTextBirthday.getText().toString());
+        intent.putExtra("Status", socialStatus);
+
+        startActivity(intent);
+    }
+
+    public void onRadioClick(View view) {
+        int id = view.getId();
+        if (id == R.id.radioStudent){
+            socialStatus = "Студент";
+        } else if (id == R.id.radioSchoolboy){
+            socialStatus = "Школьник";
+        } else if (id == R.id.radioEngineer){
+            socialStatus = "Инженер";
+        }
+    }
+
+    public void sendEmail(View view){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"university@yandex.ru"});
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
