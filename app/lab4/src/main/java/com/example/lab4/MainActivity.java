6a03bf592e8c1005f0e9031ef9823372638b778a
Nytrock <savvazhirnov@gmail.com>
2026-02-13 12:07:48 +0300

    Add lab 4 and 5 templates

--- app/lab4/src/main/java/com/example/lab4/MainActivity.java
+++ app/lab4/src/main/java/com/example/lab4/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.lab4;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        LayoutParams matchParentParam = new LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.MATCH_PARENT);
+        LayoutParams wrapContentParam = new LayoutParams(LayoutParams.WRAP_CONTENT,
+                LayoutParams.WRAP_CONTENT);
+        setContentView(linearLayout, matchParentParam);
+
+        TextView textView = new TextView(this);
+        textView.setText("TextView");
+        textView.setLayoutParams(wrapContentParam);
+        linearLayout.addView(textView);
+
+        Button button = new Button(this);
+        button.setText("Button");
+        linearLayout.addView(button, wrapContentParam);
+
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
+        Button button1 = new Button(this);
+        button1.setText("Button 1");
+        button1.callOnClick();
+        linearLayout.addView(button1, leftMarginParams);
+
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button button2 = new Button(this);
+        button2.setText("Button 2");
+        linearLayout.addView(button2, rightGravityParams);
     }
 }
--- app/lab5/src/main/java/com/example/lab5/MainActivity.java
+++ app/lab5/src/main/java/com/example/lab5/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.lab5;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,10 +15,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private final String[] names = {"Иван", "Марья", "Петр", "Антон", "Даша", "Борис", "Костя", "Игорь"};
+    private final String[] positions = {"Программист", "Бухгалтер", "Программист", "Программист",
+            "Бухгалтер", "Директор", "Программист", "Охранник"};
+    private final int[] salaries = {130000, 80000, 130000, 130000, 80000, 40000, 130000, 80000};
+    private final int[] colors = new int[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        colors[0] = Color.parseColor("#559966CC");
+        colors[1] = Color.parseColor("#55336699");
+
+        LinearLayout linearLayout = findViewById(R.id.linLayout);
+        LayoutInflater layoutInflater = getLayoutInflater();
+
+        for (int i = 0; i < names.length; i++){
+            Log.d("My logs", "i = " + i);
+
+            View item = layoutInflater.inflate(R.layout.item, linearLayout, false);
+            TextView nameView = findViewById(R.id.nameView);
+            TextView positionView = findViewById(R.id.positionView);
+            TextView salaryView = findViewById(R.id.salaryView);
+
+            nameView.setText(names[i]);
+            positionView.setText("Должность: " + positions[i]);
+            salaryView.setText("Оклад: " + salaries[i]);
+
+            item.setBackgroundColor(colors[i % 2]);
+            linearLayout.addView(item);
+        }
     }
 }
